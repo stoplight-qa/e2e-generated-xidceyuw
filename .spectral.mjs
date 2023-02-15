f6b114754c715afc3da1f416199969039ed5a115
qa-lwrhg-4-ynor@example.com <qa-lwrhg-4-ynor@example.com>
2023-02-15 00:49:28 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15269.stoplight-dev.com/api/v1/projects/cHJqOjE3NA/spectral.js?branch_slug=main&token=22aea124-1a99-4959-a9ea-e1bd3cf1f3e1";
+export default { extends: ruleset };
